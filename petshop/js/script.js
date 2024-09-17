6f1cfdc3a249b09cf88d20b74c48884705b58047
Laylacs <laylacarvalhodasilva@gmail.com>
2024-09-16 22:54:00 -0300

    Adicionando arquivos da página web

---
+++ petshop/js/script.js
@@ -0,0 +1,5 @@
+// Script para funcionalidades adicionais, como temporizadores ou interatividade extra
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Código JavaScript para funcionalidades específicas pode ser adicionado aqui
+});
